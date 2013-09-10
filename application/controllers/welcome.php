ad48a13bcdf33626f34d827b7860a6334851447d
Chris Santala <csantala@gmail.com>
2013-09-10 02:35:59 -0400

    cleanups and optimizations + source plugin icons

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,7 +4,7 @@ class Welcome extends CI_Controller {
 
 	public $reset_data = array(
 		'title' => '<h1>Welcome to the Codeigniter-CKEditor Inline Text Editor Demo!</h1>',
-		'editable1' => '<h3>Click to edit any paragraph, click outside of the edit to save to db.</h3>',
+		'editable1' => '<h3>Click any block of text to edit it, click outside of the edit field to save to db.</h3>',
 		'editable2' => '<p>Te tota aeque nobis vix, exerci libris equidem id ius, debitis alienum nam et. No illum mentitum pri, pro legimus tractatos consulatu et. Et eos oratio putant reformidans, vix fierent conceptam ex, ea eam nobis elaboraret. Duo tantas assentior ne, altera virtute repudiare nam ei. Vel nibh tincidunt id, cibo adipisci ne vel. Quaeque constituam theophrastus ei nam, cum ex exerci pertinax.</p>
 ',
 		'editable3' => '<p><img alt="" src="/images/awesome.jpg " style="float:left; height:74px; width:100px; margin-right:10px;" />
@@ -14,7 +14,8 @@ Labore mnesarchum conclusionemque ex vix. Vidit doming eos at, euismod mnesarchu
 
 	public $elements = array('title', 'editable1', 'editable2', 'editable3');
 
-	public function __construct () {
+	public function __construct ()
+	{
 		parent::__construct();
 		$this->load->database();
 	}
@@ -26,49 +27,45 @@ Labore mnesarchum conclusionemque ex vix. Vidit doming eos at, euismod mnesarchu
 
 	public function reset()
 	{
-		if (! empty($_POST)) {
-			if ($_POST['reset']) {
-				$i = 0;
-				foreach ($this->reset_data as $element => $content) {
-					$data = array(
-						'content' => $content
-					);
-					error_log($this->elements[$i]);
-					$this->db->where('content_id', $this->elements[$i]);
-					$this->db->update('content', $data);
-					$i++;
-				}
-
-			//	$this->db->update('content', $data);
-			//	error_log('insert!');
+		if (isset($_POST['reset']))
+		{
+			$i = 0;
+			foreach ($this->reset_data as $element => $content)
+			{
+				$data = array(
+					'content' => $content
+				);
+				error_log($this->elements[$i]);
+				$this->db->where('content_id', $this->elements[$i]);
+				$this->db->update('content', $data);
+				$i++;
 			}
 		}
 	}
 
 	public function update() {
 
-		if (! empty($_POST)) {
+		if (! empty($_POST))
+	 	{
 			$query = $this->db->get_where('content', array('content_id' => $_POST['content_id']));
-
-			// update if present, otherwise create a new row if unique ($conent_id)
+			// update if present, otherwise create a new row if unique ($content_id)
 			if ($query->num_rows() > 0) {
 				$data = array(
 					'date' => time(),
 					'content_id' => $_POST['content_id'],
 					'content' => $_POST['content']
 				);
-
 				$this->db->where('content_id', $_POST['content_id']);
 				$this->db->update('content', $data);
-
-			} else {
+			}
+			else
+			{
 				$data = array(
 					'id' => null,
 					'date' => time(),
 					'content_id' => $_POST['content_id'],
 					'content' => $_POST['content']
 				);
-
 				$this->db->insert('content', $data);
 			}
 		}
@@ -76,7 +73,8 @@ Labore mnesarchum conclusionemque ex vix. Vidit doming eos at, euismod mnesarchu
 		// set page content elements to default string
 		$elements = $this->elements;
 		$default = 'click to edit.';
-		foreach ($elements as $element) {
+		foreach ($elements as $element)
+		{
 			$view_data[$element] = $default;
 		}
 
@@ -84,12 +82,13 @@ Labore mnesarchum conclusionemque ex vix. Vidit doming eos at, euismod mnesarchu
 		$query = $this->db->get('content');
 		$data = $query->result();
 
-		foreach ($data as $row) {
-			if (in_array($row->content_id, $elements)) {
+		foreach ($data as $row)
+		{
+			if (in_array($row->content_id, $elements))
+			{
 				$view_data[$row->content_id] = $row->content;
 			}
 		}
-
 		$this->load->view('welcome_message', $view_data);
 	}
 }
